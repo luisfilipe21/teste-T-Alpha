d360146f8916ab23aa190fe121262c5e55b925f2
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-08-07 18:09:53 -0300

    fix: ajuste nas rotas das páginas

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router-dom"
 import { LoginForm } from "../../form/LoginForm"
 
 export const LoginPage = () => {
+    const navigate = useNavigate();
+
+    
     return (
         <section>
+            <p onClick={() => navigate("/register")}>Cadastro de usuário</p>
             <LoginForm />
         </section>
     )
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import { RegisterForm } from "../../form/RegisterForm"
 
 export const RegisterPage = () => {
+    const navigate = useNavigate();
+
     return (
         <section>
+            <p onClick={() => navigate("/")}>página de login</p>
             <RegisterForm />
         </section>
     )
